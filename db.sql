164da8c03c84778a800721ccc060d9832988ce90
mrwind-up-bird <oliver.baer@gmail.com>
2021-03-19 07:48:28 +0100

    sql

--- db.sql
+++ db.sql
@@ -7,16 +7,20 @@
  * Author:  oli
  * Created: 17.03.2021
  */
+CREATE USER 'songcontest'@'localhost' IDENTIFIED BY 'l0c4lh05t';
+CREATE DATABASE songcontest;
+GRANT ALL PRIVILEGES ON songcontest . * to 'songcontest'@'localhost';
+FLUSH PRIVILES;
 
-CREATE database `songcontest`;
-USE `songcontest`;
+USE songcontest;
 
 CREATE TABLE `history` (
   `history_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `score` int DEFAULT NULL,
-  `ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `genre` varchar(50) DEFAULT NULL,
   `contest` varchar(50) DEFAULT NULL,
+  `ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`history_id`),
   UNIQUE KEY `history_id_UNIQUE` (`history_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=158 DEFAULT CHARSET=utf8
